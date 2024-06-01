39bc502dd733b54cb3448b32b2aebf3521d840c5
SzymonG27 <s.gajdzinski123@gmail.com>
2024-06-02 00:16:13 +0200

    [TASK] - Routing for auth pages, hidden auth buttons if login or register page

--- AxiteHR.Web/src/app/core/nav-bar/nav-bar.component.ts
+++ AxiteHR.Web/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,11 +1,16 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
   animations: [
@@ -26,11 +31,32 @@ import { Component } from '@angular/core';
   ]
 })
 export class NavBarComponent {
+  isMenuOpen: boolean = false;
 
-  isMenuOpen = false;
+  currentUrl: string = "";
+  isLoginPage: boolean = false;
+  isRegisterPage: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event) => {
+        const navEndEvent = event as NavigationEnd;
+        this.currentUrl = navEndEvent.urlAfterRedirects;
+        this.checkUrl();
+      });
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+  checkUrl() {
+    this.isLoginPage = this.currentUrl.includes('Login');
+    this.isRegisterPage = this.currentUrl.includes('Register');
+  }
 }

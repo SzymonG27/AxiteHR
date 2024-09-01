4c337a2ff3ab0b8df9ce45f3d229ec44d15c4261
SzymonG27 <s.gajdzinski123@gmail.com>
2024-09-01 12:06:02 +0200

    [TECH] - ApplicationAPI config

---
+++ AxiteHR.Services.ApplicationAPI/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AxiteHR.Services.ApplicationAPI.Data
+{
+	public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+	{
+		protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+	}
+}
--- AxiteHR.Services.ApplicationAPI/Program.cs
+++ AxiteHR.Services.ApplicationAPI/Program.cs
@@ -1,11 +1,41 @@
+using AxiteHR.Services.ApplicationAPI.Data;
+using AxiteHR.Services.ApplicationAPI.Extensions;
+using AxiteHR.Services.ApplicationAPI.Helpers;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.AddGlobalization();
+builder.AddAuthentication();
+builder.Services.AddAuthorization();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(opt =>
+	opt.UseSqlServer(
+		builder.Configuration.GetConnectionString(ConfigurationHelper.DefaultConnectionString)
+	)
+);
+
+builder.Services.AddControllers()
+	.AddDataAnnotationsLocalization()
+	.AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+//Logging
+builder.AddSerilog();
+
+//Scopes, singletons
+builder.RegisterServices();
+
+//Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Cors
+builder.Services.AddCors(opt => opt.AddPolicy("NgOrigins",
+	policy => policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader())
+);
 
 var app = builder.Build();
 
@@ -19,7 +49,35 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
-app.Run();
+if (builder.Configuration.GetValue<bool>(ConfigurationHelper.IsDbFromDocker))
+{
+	using var scope = app.Services.CreateScope();
+	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	try
+	{
+		db.Database.Migrate();
+	}
+	catch (Exception ex)
+	{
+		var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+		logger.LogError(ex, "An error occurred while migrating the database.");
+	}
+}
+
+try
+{
+	Log.Information("Starting web host");
+	app.Run();
+}
+catch (Exception ex)
+{
+	Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+	Log.CloseAndFlush();
+}

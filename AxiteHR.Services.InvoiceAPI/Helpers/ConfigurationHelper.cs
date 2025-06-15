99d83ab457cb059b312a5ebf39190850e00d6a3a
SzymonG27 <s.gajdzinski123@gmail.com>
2025-06-15 22:59:39 +0200

    [TASK] #36 - Invoices API
    - Project configuration

---
+++ AxiteHR.Services.InvoiceAPI/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace AxiteHR.Services.InvoiceAPI.Helpers
+{
+	public static class ConfigurationHelper
+	{
+		public const string LogStashUrl = "LogstashConfig:LogStashUrl";
+		public const string LogStashQueueLimitBytes = "LogstashConfig:QueueLimitBytes";
+	}
+}
